9b4b37003af9c759162685e8885b658065e8023a
jay-D-Deshmukh <jaydeshmukh1212@gmail.com>
2024-05-03 19:21:25 +0530

    adding the login controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,25 +1,40 @@
-import User from '../model/user.model.js'
+import User from "../model/user.model.js";
+import bcrypt from "bcrypt";
 
-
-export const registerUser= async(req, res)=>{
-  const userdata = req.body;
+export const registerUser = async (req, res) => {
+  const {username, email, password} = req.body;
   try {
+    const hashPassword = await bcrypt.hash(password,10)
+    const datares = await User.create({
+      username,
+      email,
+      password: hashPassword
+    });
+    console.log(datares)
+    
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
-    const datares = await User.create(userdata);
-    res.status(201).json({message: "User created successfully",})
+export const loginUser = async(req, res) => {
 
+  const {email, password} = req.body;
+  try {
+    const user = await User.findOne({email});
+    if(!user){
+      return res.status(401).json({message: "Invalid credentials"});
+    }
+    const isMatch = await bcrypt.compare(password,user.password);
+    if(!isMatch){
+      return res.status(401).json({message:'Invalid credentials'})
+    }
+   res.status(200).json({message:"Login successfull"})
+  
+    
     
   } catch (error) {
-    res.status(400).json({message: error.message})
-    
+    res.status(500).json({ error: 'Internal server error' });
   }
- 
-  
-  
-     
-}
-
-export const loginUser =(req, res)=>{
-
-
-}
+};
